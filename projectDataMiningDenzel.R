28ca661a8050a4926302d59f76818ea59b78eef2
dmathew93 <denzel.mathew@gmail.com>
2015-10-11 17:39:35 -0400

    Added Iris[c45,RIPPER,Oblique,Naive]

--- projectDataMiningDenzel.R
+++ projectDataMiningDenzel.R
@@ -3,20 +3,54 @@
 
 #### Partitions the dataset into training and test data sets ######
 divideDataSet <- function(orgData){
-  set.seed(1618)
-  testData <- sample(nrow(orgData),size=nrow(orgData)*0.8, replace = FALSE)
-  trainData <- sample(nrow(orgData),size=nrow(orgData)*0.2,replace = FALSE)
-  str(testData)
-  str(trainData)
+  set.seed(6542)
+  testData <- orgData[sample(nrow(orgData),size=nrow(orgData)*0.2, replace = FALSE),]
+  trainData <- orgData[sample(nrow(orgData),size=nrow(orgData)*0.8,replace = FALSE),]
+  my_data <- list("train" = trainData, "test" = testData)
+  return(my_data)
 }
 
 ### myC45 function for iris####
-
 myIrisC45 <- function(){
   library(RWeka)
-  dataset <- divideDataSet(iris)
-  fit <- J48(Species~.,data=dataset$testData)
+  dataset <- divideDataSet(iris)$train
+  fit <- J48(Species~.,data=dataset)
+  myC45predict(fit)
 }
 
-### my c45 predict function for iris ####
+### my c45 predict function for iris ###
+myC45predict <- function(fit){
+  library(RWeka)
+  testData <- divideDataSet(iris)$test
+  predictions <- predict(fit,testData)
+  summary(fit,newdata=testData,class=TRUE)
+  #table(predictions)
+}
+
+#### my iris RIPPER  ######
+myIrisRIPPER <- function(){
+  library(RWeka)
+  dataset <- divideDataSet(iris)$train
+  JRipFit <- JRip(Species~.,dataset)
+  myC45predict(JRipFit)
+}
+
+
+### My Iris Oblique #####
+myIrisOblique <- function(){
+  library(oblique.tree)
+  dataset <- divideDataSet(iris)$train
+  ObliqueShit <- oblique.tree(Species~.,dataset)
+  myC45predict(ObliqueShit)
+}
+
+### My Iris Naive Bayes ####
+myNaiveBayes <- function(){
+  library(e1071)
+  dataset <- divideDataSet(iris)$train
+  naiveShit <- naiveBayes(Species~.,dataset)
+  myC45predict(naiveShit)
+}
+
+
 
