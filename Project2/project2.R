4654b783fa115cc7adde736dda18f1d2f07a9436
dmathew93 <denzel.mathew@gmail.com>
2015-10-24 11:52:11 -0400

    finished titanic

--- Project2/project2.R
+++ Project2/project2.R
@@ -1,17 +1,21 @@
 library(arules)
 
 ###### Read in Datasets #########
-titanic <- read.csv("C://Users//Denzel//Desktop//titanic.csv")
-retail  <- read.csv("C://Users//Denzel//Desktop//retail.csv")
-GoT     <- read.csv("C://Users//Denzel//Desktop/game_of_thrones.csv")
+titanic <- read.csv("C://Users//Denzel//Desktop//DataMining//Project2//titanic.csv")
+retail  <- read.csv("C://Users//Denzel//Desktop//DataMining//Project2//retail.csv")
+GoT     <- read.csv("C://Users//Denzel//Desktop//DataMining//Project2//game_of_thrones.csv")
 
 
 ######  List all rules   #########
-titanic_all <- apriori(titanic,parameter=list(support=0,confidence=0));
+titanic_all <- apriori(titanic,parameter=list(support=0,confidence=0),
+                       appearance=list(rhs=c("Survived=No","Survived=Yes"),
+                       default="lhs"));
 
 
 #####   Filter out rules #########
-titanic_filtered <- apriori(titanic,parameter=list(support=0.01,confidence=0.9));
+titanic_filtered <- apriori(titanic,parameter=list(support=0.01,confidence=0.9),
+                         appearance=list(rhs=c("Survived=Yes","Survived=No"),
+                                         default="lhs"));
 
 
 #####   Redundant Rules ########
@@ -19,7 +23,21 @@ subset.matrix <- is.subset(titanic_filtered,titanic_filtered)
 subset.matrix[lower.tri(subset.matrix,diag=T)] <- NA
 redundant <- colSums(subset.matrix,na.rm=T) >= 1
 rules <- titanic_filtered[!redundant]
-inspect(rules);
+
+#######   Retail  #############
+
+retail_all <- apriori(retail,parameter=list(support=0,confidence=0));
+
+
+######  List all rules   #########
+
+
+
+
+
+
+
+
 
 
 ####### Game of Thrones ########
