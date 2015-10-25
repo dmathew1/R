6c6d89a62bb1f111595f2e36e168766d0b95a671
dmathew93 <denzel.mathew@gmail.com>
2015-10-24 22:51:06 -0400

    Finished Retail

--- Project2/project2.R
+++ Project2/project2.R
@@ -9,7 +9,7 @@ GoT     <- read.csv("C://Users//Denzel//Desktop//DataMining//Project2//game_of_t
 ######  List all rules   #########
 titanic_all <- apriori(titanic,parameter=list(support=0,confidence=0),
                        appearance=list(rhs=c("Survived=No","Survived=Yes"),
-                       default="lhs"));
+                                       default="lhs"));
 
 
 #####   Filter out rules #########
@@ -24,12 +24,32 @@ subset.matrix[lower.tri(subset.matrix,diag=T)] <- NA
 redundant <- colSums(subset.matrix,na.rm=T) >= 1
 rules <- titanic_filtered[!redundant]
 
-#######   Retail  #############
-
-retail_all <- apriori(retail,parameter=list(support=0,confidence=0));
+retailFunc <- function(){
+#######   Retail PreProcessing  #############
+retail$TransactionNo <- NULL
+retail <- sapply(retail,as.logical)
 
 
 ######  List all rules   #########
+retail_all <- apriori(retail,parameter = list(support=0,confidence=0),
+                           appearance = list(rhs=c("Beverage","PersonalCare","Meat"),
+                                             default="lhs"));
+
+#####   Filtered Rules  #########
+retail_filtered <- apriori(retail,parameter = list(support=0.01,confidence=0.9),
+                           appearance = list(rhs=c("Beverage","Meat","PersonalCare"),
+                                             default="lhs"));
+
+
+#####   Redundant Rules ########
+subset.matrix <- is.subset(retail_filtered,retail_filtered)
+subset.matrix[lower.tri(subset.matrix,diag=T)] <- NA
+redundant <- colSums(subset.matrix,na.rm=T) >= 1
+retail_rules <- retail_filtered[!redundant]
+return(inspect(retail_rules))
+}
+
+
 
 
 
